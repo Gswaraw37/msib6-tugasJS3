52beafbccc8bb553c4f2cad85bdec2df499c1857
Gswaraw37 <genta.swara@gmail.com>
2024-03-15 10:55:37 +0700

    tugas JS ketiga

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function dataPerson() {
+    //tugasnya adalah lengkapi kode berikut dan tampilkan hasilnya ada pada HTML
+    //1. isian nama
+    //2. isian pekerjaan
+    //3. isian hobby
+    let forms = document.getElementById('frm');
+    let nama = forms.nama.value;
+    let pekerjaan = forms.pekerjaan.value;
+    let hobi = forms.hobi.value;
+    let input = 'Selamat Pagi ' + nama + '<br>Kamu Adalah Seorang ' + pekerjaan + '<br>Hobi Kamu Adalah ' + hobi;
+    let no_input = 'Harap Lengkapi Form Terlebih Dahulu';
+    let hasil = (nama && pekerjaan && hobi != '') ? input : no_input;
+
+    document.getElementById('hasil').innerHTML = hasil;
+}
